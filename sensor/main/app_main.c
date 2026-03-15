2b44fbfb2c7f298f73658c96f51f3f7e29c31bbf
joppe <jboeve009@gmail.com>
2026-03-15 18:37:12 +0100

    Add sensor

---
+++ sensor/main/app_main.c
@@ -0,0 +1,162 @@
+#include "esp_wifi_types_generic.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+
+#include "esp_log.h"
+#include "esp_wifi.h"
+#include <stdint.h>
+#include <stdio.h>
+#include <string.h>
+#include <sys/unistd.h>
+
+#if ESP_IDF_VERSION >= ESP_IDF_VERSION_VAL(4, 4, 0)
+#include "esp_mac.h"
+#endif
+
+#include "espnow.h"
+#include "espnow_storage.h"
+#include "espnow_utils.h"
+
+#include "driver/uart.h"
+
+// You can modify these according to your boards.
+#define UART_BAUD_RATE 115200
+#define UART_PORT_NUM  0
+#define UART_TX_IO     UART_PIN_NO_CHANGE
+#define UART_RX_IO     UART_PIN_NO_CHANGE
+
+static const uint8_t MOTHERSHIP_MAC[6] = {0x7F, 0x9E, 0xBD, 0x39, 0x7A, 0xD8};
+
+static const char *TAG = "app_main";
+
+static void app_uart_read_task(void *arg)
+{
+    esp_err_t ret = ESP_OK;
+    uint32_t count = 0;
+    size_t size = 0;
+    uint8_t *data = ESP_CALLOC(1, ESPNOW_DATA_LEN);
+
+    ESP_LOGI(TAG, "Uart read handle task is running");
+
+    espnow_frame_head_t frame_head = {
+        .retransmit_count = CONFIG_RETRY_NUM,
+        .broadcast = true,
+    };
+
+    for (;;)
+    {
+        size = uart_read_bytes(UART_PORT_NUM, data, ESPNOW_DATA_LEN, pdMS_TO_TICKS(10));
+        ESP_ERROR_CONTINUE(size <= 0, "");
+
+        ret = espnow_send(ESPNOW_DATA_TYPE_DATA, ESPNOW_ADDR_BROADCAST, data, size, &frame_head, portMAX_DELAY);
+        ESP_ERROR_CONTINUE(ret != ESP_OK, "<%s> espnow_send", esp_err_to_name(ret));
+
+        ESP_LOGI(TAG, "espnow_send, count: %" PRIu32 ", size: %u, data: %s", count++, size, data);
+        memset(data, 0, ESPNOW_DATA_LEN);
+    }
+
+    ESP_LOGI(TAG, "Uart handle task is exit");
+
+    ESP_FREE(data);
+    vTaskDelete(NULL);
+}
+
+static void app_uart_initialize()
+{
+    uart_config_t uart_config = {
+        .baud_rate = UART_BAUD_RATE,
+        .data_bits = UART_DATA_8_BITS,
+        .parity = UART_PARITY_DISABLE,
+        .stop_bits = UART_STOP_BITS_1,
+        .flow_ctrl = UART_HW_FLOWCTRL_DISABLE,
+#if SOC_UART_SUPPORT_REF_TICK
+        .source_clk = UART_SCLK_REF_TICK,
+#elif SOC_UART_SUPPORT_XTAL_CLK
+        .source_clk = UART_SCLK_XTAL,
+#endif
+    };
+
+    ESP_ERROR_CHECK(uart_param_config(UART_PORT_NUM, &uart_config));
+    ESP_ERROR_CHECK(uart_set_pin(UART_PORT_NUM, UART_TX_IO, UART_RX_IO, UART_PIN_NO_CHANGE, UART_PIN_NO_CHANGE));
+    ESP_ERROR_CHECK(uart_driver_install(UART_PORT_NUM, 8 * ESPNOW_DATA_LEN, 8 * ESPNOW_DATA_LEN, 0, NULL, 0));
+
+    // xTaskCreate(app_uart_read_task, "app_uart_read_task", 4 * 1024, NULL, tskIDLE_PRIORITY + 1, NULL);
+}
+
+static void app_wifi_init()
+{
+    esp_event_loop_create_default();
+
+    wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
+
+    ESP_ERROR_CHECK(esp_wifi_init(&cfg));
+    ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_STA));
+    ESP_ERROR_CHECK(esp_wifi_set_storage(WIFI_STORAGE_RAM));
+    ESP_ERROR_CHECK(esp_wifi_set_ps(WIFI_PS_NONE));
+    ESP_ERROR_CHECK(esp_wifi_start());
+}
+
+static esp_err_t receive_handle(uint8_t *src_addr, void *data, size_t size, wifi_pkt_rx_ctrl_t *rx_ctrl)
+{
+    ESP_PARAM_CHECK(src_addr);
+    ESP_PARAM_CHECK(data);
+    ESP_PARAM_CHECK(size);
+    ESP_PARAM_CHECK(rx_ctrl);
+
+    static uint32_t count = 0;
+
+    ESP_LOGI(TAG, "espnow_recv, <%" PRIu32 "> [" MACSTR "][%d][%d][%u]: %.*s", count++, MAC2STR(src_addr),
+             rx_ctrl->channel, rx_ctrl->rssi, size, size, (char *)data);
+
+    return ESP_OK;
+}
+
+void send_message()
+{
+    uint32_t count = 0;
+    const size_t size = sizeof(uint32_t);
+
+    esp_err_t ret = ESP_OK;
+
+    espnow_frame_head_t frame_head = {
+        .retransmit_count = CONFIG_RETRY_NUM,
+        .broadcast = false,
+    };
+
+    while (1)
+    {
+        ret = espnow_send(ESPNOW_DATA_TYPE_DATA, MOTHERSHIP_MAC, &count, size, &frame_head, portMAX_DELAY);
+        if (ret != ESP_OK)
+            ESP_LOGE(TAG, "<%s> espnow_send", esp_err_to_name(ret));
+
+        ESP_LOGI(TAG, "espnow_send, size: %u, data: %u", size, count);
+        count++;
+
+        vTaskDelay(1500 / portTICK_PERIOD_MS);
+    }
+    vTaskDelete(NULL);
+}
+
+void app_main()
+{
+    // TODO: Add Banner telling bteween sensor/mothership it is
+    espnow_storage_init();
+
+    app_uart_initialize();
+    app_wifi_init();
+
+    espnow_config_t espnow_config = ESPNOW_INIT_CONFIG_DEFAULT();
+    espnow_init(&espnow_config);
+
+    // Get rid of broadcast peer, this is set by default in `espnow_init`.
+    espnow_del_peer(ESPNOW_ADDR_BROADCAST);
+#ifndef WW_MOTHERSHIP
+    espnow_add_peer(MOTHERSHIP_MAC, NULL);
+#endif
+
+    espnow_set_config_for_data_type(ESPNOW_DATA_TYPE_DATA, true, receive_handle);
+
+#ifndef WW_MOTHERSHIP
+    xTaskCreate(send_message, "send_message", 4 * 1024, NULL, tskIDLE_PRIORITY + 1, NULL);
+#endif
+}
