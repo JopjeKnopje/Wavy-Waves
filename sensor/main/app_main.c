6eab4523d0d432628664a4bcc5da4b850b0c31dd
joppe <jboeve009@gmail.com>
2026-03-15 23:42:51 +0100

    WIP: Trying to get ack to work

--- sensor/main/app_main.c
+++ sensor/main/app_main.c
@@ -1,3 +1,4 @@
+#include "esp_now.h"
 #include "esp_wifi_types_generic.h"
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
@@ -44,6 +45,8 @@ void send_message()
     espnow_frame_head_t frame_head = {
         .retransmit_count = CONFIG_RETRY_NUM,
         .broadcast = false,
+		// TODO: when enabling this it fails
+		.ack = true,
     };
 
     while (1)
@@ -51,6 +54,7 @@ void send_message()
         ret = espnow_send(ESPNOW_DATA_TYPE_DATA, MOTHERSHIP_MAC, &count, size, &frame_head, portMAX_DELAY);
         if (ret != ESP_OK)
             ESP_LOGE(TAG, "<%s> espnow_send", esp_err_to_name(ret));
+
 
         ESP_LOGI(TAG, "espnow_send, size: %u, data: %u", size, count);
         count++;
