51af238cda6893196053c8f399339c39d948b775
joppe <jboeve009@gmail.com>
2026-03-20 23:40:36 +0100

    Add more MACs

--- common/ww_data.h
+++ common/ww_data.h
@@ -3,6 +3,12 @@
 
 #include "espnow.h"
 
-static const espnow_addr_t MOTHERSHIP_MAC = {0x10, 0x52, 0x1C, 0x69, 0x08, 0xD8};
+static const espnow_addr_t DEV_1_MAC = {0x7C, 0x9e, 0xBD, 0xF9, 0xD0, 0x58};
+static const espnow_addr_t DEV_2_MAC = {0x10, 0x52, 0x1C, 0x69, 0x08, 0xD8};
+static const espnow_addr_t DEV_3_MAC = {0x7C, 0x9E, 0xBD, 0xf4, 0x4E, 0xB4};
+// TODO: Add ID-4 MAC addr
+// static const espnow_addr_t DEV_4_MAC = {};
+
+#define MOTHERSHIP_MAC DEV_2_MAC
 
 #endif
